84d777e1e9e185d30124eecbf45f953ce88d31c8
István Derda <istvan.derda@mailbox.org>
2025-07-30 07:54:43 +0000

    set args.num_workers default to to

--- cfg.py
+++ cfg.py
@@ -85,7 +85,7 @@ def parse_args():
     parser.add_argument(
         '--num_workers',
         type=int,
-        default=8,
+        default=2,
         help='number of cpu threads to use during batch generation')
     parser.add_argument(
         '--latent_dim',
